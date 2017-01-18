cb2dc548a4c00734deb0a190691704c1ace68eaf
Prince <prinheir9@gmail.com>
2017-01-18 20:15:39 +0200

    gradle changes

--- app/src/main/java/appr/kulaf/com/kulaapp/FoodActivity.java
+++ app/src/main/java/appr/kulaf/com/kulaapp/FoodActivity.java
@@ -2,14 +2,25 @@ package appr.kulaf.com.kulaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class FoodActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private String URL = "";
+    private List<Food_model> foods;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+
+
+
     }
 }
