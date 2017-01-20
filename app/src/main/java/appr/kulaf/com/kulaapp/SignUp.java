fd9421b3bb5f7a2ef496df9ce090467f0056c27e
Kulafine <kulafine@gmail.com>
2017-01-20 08:04:06 -0800

    registration

--- app/src/main/java/appr/kulaf/com/kulaapp/SignUp.java
+++ app/src/main/java/appr/kulaf/com/kulaapp/SignUp.java
@@ -72,7 +72,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                try {
-                   String result = response.getString("result");
+                   String result = response.getString("result").toString();
                    if (result == "200"){
                        Intent intent = new Intent(getApplicationContext(),Login.class);
                        startActivity(intent);
