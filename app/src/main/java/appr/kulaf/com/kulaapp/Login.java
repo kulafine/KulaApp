869fbebfe4da9d07d44f0d7e970e4ac1c106295e
Kulafine <kulafine@gmail.com>
2017-01-26 13:16:16 -0800

    registration

--- app/src/main/java/appr/kulaf/com/kulaapp/Login.java
+++ app/src/main/java/appr/kulaf/com/kulaapp/Login.java
@@ -4,18 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Hashtable;
-import java.util.Map;
 
 public class Login extends AppCompatActivity {
     public EditText username;
@@ -28,39 +16,39 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-       // username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        login = (Button) findViewById(R.id.loginbtn);
-
-    }
-    public void login(){
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, Url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-            try {
-                String name = response.getString("result");
-                if (name == "200"){
-                    Toast.makeText(getApplicationContext(),"successful loogein",Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(getApplicationContext(),"fail to gin",Toast.LENGTH_LONG).show();
-                }
-
-            }catch (JSONException E){
-
-            }
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        Map<String, String> params = new Hashtable<String, String>();
-
-
+//       // username = (EditText)findViewById(R.id.username);
+//        password = (EditText)findViewById(R.id.password);
+//        login = (Button) findViewById(R.id.loginbtn);
+//
+//    }
+//    public void login(){
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, Url, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//            try {
+//                String name = response.getString("result");
+//                if (name == "200"){
+//                    Toast.makeText(getApplicationContext(),"successful loogein",Toast.LENGTH_LONG).show();
+//                }else {
+//                    Toast.makeText(getApplicationContext(),"fail to gin",Toast.LENGTH_LONG).show();
+//                }
+//
+//            }catch (JSONException E){
+//
+//            }
+//
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+//        Map<String, String> params = new Hashtable<String, String>();
+//
+//
 
     }
 }
--- app/src/main/java/appr/kulaf/com/kulaapp/SignUp.java
+++ app/src/main/java/appr/kulaf/com/kulaapp/SignUp.java
@@ -1,21 +1,10 @@
 package appr.kulaf.com.kulaapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class SignUp extends AppCompatActivity {
 
@@ -36,81 +25,81 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        username = (EditText)findViewById(R.id.username);
-        email = (EditText) findViewById(R.id.mail);
-        password = (EditText) findViewById(R.id.password);
-
-        signup = (Button)findViewById(R.id.signup);
-        textView = (TextView)findViewById(R.id.text);
-
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Login.class);
-              startActivity(intent);
-            }
-        });
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerTheUser();
-            }
-        });
-
-
-
-    }
-    private void registerTheUser(){
-        final String EDusername = username.getText().toString().trim();
-        final  String EDpassword = password.getText().toString().trim();
-        final String EDmail = email.getText().toString().trim();
-       JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put(KEY_USERNAME,EDusername);
-            jsonObject.put(KEY_PASSWORD,EDpassword);
-            jsonObject.put(KEY_EMAIL,EDmail);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,Url, jsonObject, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                textView.setText(response.toString());
-//               try {
-//                   String result = response.getString("result").toString();
-//                   if (result == "200"){
-//                       Intent intent = new Intent(getApplicationContext(),Login.class);
-//                       startActivity(intent);
-//                   }else if(result == "0") {
-//                       Toast.makeText(getApplicationContext(),"the user exist",Toast.LENGTH_LONG).show();
-//                   }
-//               }catch (JSONException e){
-//                   Toast.makeText(getApplicationContext(),"error while parsing ",Toast.LENGTH_LONG).show();
-//               }
-//                da
+//        username = (EditText)findViewById(R.id.username);
+//        email = (EditText) findViewById(R.id.mail);
+//        password = (EditText) findViewById(R.id.password);
 //
+//        signup = (Button)findViewById(R.id.signup);
+//        textView = (TextView)findViewById(R.id.text);
 //
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(),"error while parsing "+error,Toast.LENGTH_LONG).show();
-            }
-       });
-// {
+//        signin.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            protected Map<String, String> getHeaders() throws AuthFailureError {
-//
-//                Map<String, String> params = new HashMap<String, String>();
-//                params.put(KEY_USERNAME, EDusername);
-//                params.put(KEY_PASSWORD,EDpassword);
-//                params.put(KEY_EMAIL,EDmail);
-//                return params;
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(),Login.class);
+//              startActivity(intent);
 //            }
-//        };
-        Server.getServer_instance(getApplicationContext()).addRequest(jsonObjectRequest);
+//        });
+//        signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                registerTheUser();
+//            }
+//        });
+//
+//
+//
+//    }
+//    private void registerTheUser(){
+//        final String EDusername = username.getText().toString().trim();
+//        final  String EDpassword = password.getText().toString().trim();
+//        final String EDmail = email.getText().toString().trim();
+//       JSONObject jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put(KEY_USERNAME,EDusername);
+//            jsonObject.put(KEY_PASSWORD,EDpassword);
+//            jsonObject.put(KEY_EMAIL,EDmail);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,Url, jsonObject, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                textView.setText(response.toString());
+////               try {
+////                   String result = response.getString("result").toString();
+////                   if (result == "200"){
+////                       Intent intent = new Intent(getApplicationContext(),Login.class);
+////                       startActivity(intent);
+////                   }else if(result == "0") {
+////                       Toast.makeText(getApplicationContext(),"the user exist",Toast.LENGTH_LONG).show();
+////                   }
+////               }catch (JSONException e){
+////                   Toast.makeText(getApplicationContext(),"error while parsing ",Toast.LENGTH_LONG).show();
+////               }
+////                da
+////
+////
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getApplicationContext(),"error while parsing "+error,Toast.LENGTH_LONG).show();
+//            }
+//       });
+//// {
+////            @Override
+////            protected Map<String, String> getHeaders() throws AuthFailureError {
+////
+////                Map<String, String> params = new HashMap<String, String>();
+////                params.put(KEY_USERNAME, EDusername);
+////                params.put(KEY_PASSWORD,EDpassword);
+////                params.put(KEY_EMAIL,EDmail);
+////                return params;
+////            }
+////        };
+//        Server.getServer_instance(getApplicationContext()).addRequest(jsonObjectRequest);
 
 
     }
